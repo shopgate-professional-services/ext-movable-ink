3fa19226fa30426ae789530b6e38d0a431985680
aylinuenal <aylin.uenal@shopgate.com>
2024-08-21 14:08:59 +0200

    CURB-4302 fetch customer data for movable ink widget

--- frontend/widgets/MovableInkWidget/index.jsx
+++ frontend/widgets/MovableInkWidget/index.jsx
@@ -1,35 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Handlebars from "handlebars/dist/handlebars";
+import Handlebars from 'handlebars/dist/handlebars';
+import { fetchCustomerData } from '@shopgate/engage/account/actions/fetchCustomer';
+import { connect } from 'react-redux';
+import { isUserLoggedIn } from '@shopgate/engage/user';
+import { getCustomer } from '@shopgate/engage/account/selectors/customer';
 
 /**
+ * Maps state to props.
+ * @param {Object} state State.
+ * @param {Object} props Component props.
+ * @returns {Object}
+ */
+const mapStateToProps = (state, props) => ({
+  isLoggedIn: isUserLoggedIn(state, props),
+  customer: getCustomer(state),
+});
+
+/**
+ * @param {Function} dispatch Dispatch.
+ * @returns {Object}
+ */
+const mapDispatchToProps = dispatch => ({
+  fetchCustomer: () => dispatch(fetchCustomerData()),
+});
+
+/**
+ * @param {Object} settings the settings for the widget
+ * @param {Function} fetchCustomer fetch current customer
+ * @param {boolean} isLoggedIn whether customer is logged in
+ * @param {Object} customer the customer data
  * @returns {JSX.Element}
  */
-const MovableInkWidget = ({ settings }) => {
-  const {imageUrl, linkUrl} = settings;
+const MovableInkWidget = ({
+  settings, fetchCustomer, isLoggedIn, customer,
+}) => {
+  const { imageUrl, linkUrl } = settings;
 
-  if (!imageUrl || !linkUrl) {
+  useEffect(() => {
+    if (isLoggedIn) {
+      fetchCustomer();
+    }
+  }, [fetchCustomer, isLoggedIn]);
+
+  if (!imageUrl || !linkUrl || !customer) {
     return null;
   }
 
-  const customerDataMock = {
-    firstName: 'Anna',
-    lastName: 'Minnie',
-    externalCustomerNumber: 123,
-  };
-
-  const template = Handlebars.compile( imageUrl );
-  const parsedUrl = template({customer: customerDataMock});
+  const template = Handlebars.compile(imageUrl, { options: { noEscape: true } });
+  const parsedUrl = template({ customer });
 
   return (
     <div>
-    <h2>Movable ink widget!!!!!</h2>
-    <img src={parsedUrl} />
+      <img src={parsedUrl} alt="" />
     </div>
-  )
+  );
 };
 
 MovableInkWidget.propTypes = {
+  fetchCustomer: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  customer: PropTypes.shape(),
   settings: PropTypes.shape({
     imageUrl: PropTypes.string,
     linkUrl: PropTypes.string,
@@ -38,6 +69,7 @@ MovableInkWidget.propTypes = {
 
 MovableInkWidget.defaultProps = {
   settings: null,
+  customer: null,
 };
 
-export default MovableInkWidget;
+export default connect(mapStateToProps, mapDispatchToProps)(MovableInkWidget);
